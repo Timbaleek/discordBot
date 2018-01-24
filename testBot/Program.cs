54028e11ae13821d5a50d119d9400c3b58449744
Timbaleek <timbaleek@hotmail.de>
2018-01-24 23:07:03 +0100

    whisper

--- testBot/Program.cs
+++ testBot/Program.cs
@@ -152,7 +152,10 @@ namespace testBot
                         await context.Channel.SendFileAsync(imgs2[rnd2.Next(0, imgs2.Length)]);
                         break;
                     case "!clear":
-                        //context.Channel.DeleteMessagesAsync(parts[1]);
+                        context.Channel.DeleteMessagesAsync(parts[1]);
+                        break;
+                    case "!whisper":
+                        await msg.Author.SendMessageAsync(msg.Author.Username + " whispered to you: " + parts[1]);
                         break;
                     case "!help":
                         String helpString = "";
